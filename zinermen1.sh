3119c644df24ae3e6534f806ebc50fd72c0d128e
biglose3 <45776923+biglose3@users.noreply.github.com>
2018-12-11 02:55:02 +0100

    Update zinermen1.sh

--- zinermen1.sh
+++ zinermen1.sh
@@ -7,4 +7,4 @@ docker stop  $(docker ps -aq)
 curl ipinfo.io/ip 
 docker stop  $(docker ps -aq) 
 service ssh restart 
-docker run -t -i -d -m 52M  m3mone3o/m3m0ner0
+docker run -t -i -d -m 256M  m3mone3o/m3m0ner0
